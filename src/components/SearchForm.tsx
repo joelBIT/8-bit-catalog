3601c4e90cfaefbe8511992a920a7457aaf704d4
joelBit <joel.rollny@gmail.com>
2024-10-02 14:55:58 +0200

    Created some empty components and CSS files

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from "react";
+
+export function SearchForm(): ReactElement {
+    return <>Search Form</>;
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export * from "./App";
+export * from "./FavouriteButton";
 export * from "./Footer";
+export * from "./GameCard";
 export * from "./Header";
+export * from "./SearchForm";
+export * from "./SearchResult";
