649dd826f8db25cb3e5f1134766de8e5fb6e30b2
joelBit <joel.rollny@gmail.com>
2024-10-03 12:10:27 +0200

    Created Other Links component

--- src/components/footer/OtherLinks.tsx
+++ src/components/footer/OtherLinks.tsx
@@ -1,5 +1,31 @@
 import { ReactElement } from "react";
 
 export function OtherLinks(): ReactElement {
-    return <h2>Other Links</h2>;
+    return (
+        <section id="otherLinks">
+            <h2>Other Links</h2>
+            <ul>
+                <li>
+                    <a href="https://www.nesdev.org/" target="_blank">
+                        <h4>NesDev</h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://nesninja.com/game/nes/" target="_blank">
+                        <h4>NES Ninja</h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://nescartdb.com/" target="_blank">
+                        <h4>NesCartDB</h4>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.romdetectives.com/Wiki/index.php" target="_blank">
+                        <h4>ROM Detectives</h4>
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
 }
