f87e931a0c42376adae9f8e580ac7ae931fff071
joelBit <joel.rollny@gmail.com>
2024-10-03 15:24:01 +0200

    Created Login Form component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,16 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 export function LoginForm(): ReactElement {
-    return <>Login Form</>;
+    return (
+        <section id="loginCard">
+            <h1>Log in</h1>
+            <form id="loginForm">
+                <input id="loginUsername" type="text" placeholder="Username" required />
+                <input id="loginPassword" type="password" placeholder="Password" autoComplete="false" required />
+                <button onClick={() => console.log('log in')}>Login</button>
+            </form>
+            <Link to="/register">Create an Account</Link>
+        </section>
+    );
 }
