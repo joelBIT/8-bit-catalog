1d215fcc5a715f08ff42ac54213163ad4c778e02
joelBit <joel.rollny@gmail.com>
2024-10-03 11:40:34 +0200

    Created Site Links component

--- src/components/footer/SiteLinks.tsx
+++ src/components/footer/SiteLinks.tsx
@@ -1,5 +1,27 @@
 import { ReactElement } from "react";
+import { NavLink } from "react-router-dom";
 
 export function SiteLinks(): ReactElement {
-    return <h2>Site Links</h2>;
+    return (
+        <section id="siteLinks">
+            <h2>Site Links</h2>
+            <ul>
+                <li>
+                    <NavLink to="/">
+                        <h4>Home</h4>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">
+                        <h4>About</h4>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login">
+                        <h4>Account</h4>
+                    </NavLink>
+                </li>
+            </ul>
+        </section>
+    );
 }
