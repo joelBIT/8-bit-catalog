96b7162ff67737b6bdec4d277f4f578c9d99097d
joelBit <joel.rollny@gmail.com>
2024-10-03 09:16:04 +0200

    Styled the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { ReactElement } from "react";
 import { Logo, NavBar } from ".";
+import { NavLink } from "react-router-dom";
 
 export function Header(): ReactElement {
     return (
         <header>
-            <Logo />
+            <NavLink to="/">
+                <Logo />
+            </NavLink>
             <NavBar />
         </header>
     );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,31 @@
 import { ReactElement } from "react";
+import { NavLink } from "react-router-dom";
 
 export function NavBar(): ReactElement {
     return (
         <nav id="navbar">
-            <h1>NavBar</h1>
+            <ul>
+                <li>
+                    <NavLink to="/games">
+                        <h2>Games</h2>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/request">
+                        <h2>Request</h2>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/favourites">
+                        <h2>Favourites</h2>
+                    </NavLink>
+                </li>
+                <li className="loginIconElement">
+                    <NavLink className="loginLink" to="/login">
+                        <span className="material-symbols-outlined">login</span>
+                    </NavLink>
+                </li>
+            </ul>
         </nav>
     );
 }
