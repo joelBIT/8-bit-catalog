5f8fa84e0dd1b46f088924f34f1e9373e96398ca
joelBit <joel.rollny@gmail.com>
2024-10-06 09:36:58 +0200

    Search using filters

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,11 +1,14 @@
-import { ReactElement } from "react";
+import { ReactElement, useRef } from "react";
+import { DEFAULT_OPTION_VALUE } from "../utils";
 
-export function Select({title, list}: {title: string, list: string[]}): ReactElement {
+export function Select({title, list, getOption}: {title: string, list: string[], getOption: Function}): ReactElement {
+    const ref = useRef<HTMLSelectElement>(null);
+
     return (
         <section>
             <h2>{title}</h2>
-            <select>
-                <option value="All">All</option>
+            <select ref={ref} onChange={() => getOption(ref.current?.value)}>
+                <option value={DEFAULT_OPTION_VALUE}>{DEFAULT_OPTION_VALUE}</option>
                 {list.map((element, index) => <option key={index} value={element}>{element}</option>)}
             </select>
         </section>
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,16 +1,35 @@
 import { ReactElement, useState } from "react";
 import { SearchForm, SearchResult } from "../components";
 import { Game } from "../interfaces";
+import { DEFAULT_OPTION_VALUE } from "../utils";
 
 export function SearchPage(): ReactElement {
     const [searchResult, setSearchResult] = useState<Game[]>([]);
     const [showHeading, setShowHeading] = useState(false);
     
-    function search(query: string) {
+    function search(title: string, category: string, publisher: string, developer: string) {
         const games = localStorage.getItem("games") || "[]";
-        const result = JSON.parse(games).filter((game: { [x: string]: any; }) => game.title.toLowerCase().includes(query.toLocaleLowerCase()));
+        let result = JSON.parse(games);
+        if (category !== DEFAULT_OPTION_VALUE) {
+            result = filter(result, "category", category);
+        }
+
+        if (publisher !== DEFAULT_OPTION_VALUE) {
+            result = filter(result, "publisher", publisher);
+        }
+
+        if (developer !== DEFAULT_OPTION_VALUE) {
+            result = filter(result, "developer", developer);
+        }
+
+        result = filter(result, "title", title);
+        
         setShowHeading(true);
         setSearchResult(result);
+    }
+
+    function filter(list: Game[], filter: string, value: string) {
+        return list.filter((game: { [x: string]: any; }) => game[filter]?.toLowerCase().includes(value.toLocaleLowerCase()));
     }
 
     return (
