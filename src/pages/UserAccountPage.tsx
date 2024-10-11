a8f6b28a4f75562a06bb753a5e1257cf1afc9413
joelBit <joel.rollny@gmail.com>
2024-10-11 16:28:17 +0200

    Removed unnecessary code

--- src/pages/UserAccountPage.tsx
+++ src/pages/UserAccountPage.tsx
@@ -1,20 +1,8 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement } from "react";
 import { AccountTabs } from "../components/account/AccountTabs";
-import { useNavigate, useParams } from "react-router-dom";
 import { getActiveUser } from "../data";
 
 export function UserAccountPage(): ReactElement {
-    const navigate = useNavigate();
-    let { id } = useParams<string>();
-
-    /**
-     * Navigate to Forbidden Page if the current user ID is different than the ID supplied in params.
-     */
-    useEffect(() => {
-        if (id && getActiveUser().id !== parseInt(id)) {
-            navigate("/403");
-        }
-    });
 
     return (
         <main id="accountPage">
