12f06a03608fd61064572c0eee7c5cc6307d67a6
joelBit <joel.rollny@gmail.com>
2024-10-12 10:08:19 +0200

    Removed unnecessary code

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -1,19 +1,15 @@
-import { ChangeEvent, ReactElement, useRef } from "react";
+import { ChangeEvent, ReactElement } from "react";
 
 export function DateInput({ id, label, value, setDate }: { id: string, label: string, value: string, setDate: (arg0: string) => void}): ReactElement {
-    const ref = useRef<HTMLInputElement>(null);
     
     function handleDate(event: ChangeEvent<HTMLInputElement>) {
-        console.log(ref.current?.value);
-        console.log(event.target.value);
         setDate(event.target.value);
-        //setDate(parseInt(event.target.value.slice(0, 4)));
     }
 
     return (
         <section id="releasedSection">
             <h2>{label}</h2>
-            <input id={id} type="date" ref={ref} value={value} onChange={handleDate} required />
+            <input id={id} type="date" value={value} onChange={handleDate} required />
         </section>
     );
 }
