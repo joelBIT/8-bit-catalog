1e8203b5aa08dfb77718ee95821a03799a774a33
joelBit <joel.rollny@gmail.com>
2024-10-12 10:16:35 +0200

    loggedin user set in context when rerendering page

--- src/contexts/ProtectedRouteContextProvider.tsx
+++ src/contexts/ProtectedRouteContextProvider.tsx
@@ -1,11 +1,11 @@
 import { createContext, ReactElement, useState } from "react";
 import { AuthContextProvider, ContextProviderChildren, User } from "../interfaces";
-import { createAnonymousUser } from "../data";
+import { activeUserExists, createAnonymousUser, getActiveUser } from "../data";
 
 export const AuthContext = createContext<AuthContextProvider>({} as AuthContextProvider);
 
 export function ProtectedRouteContextProvider({children}: ContextProviderChildren): ReactElement {
-    const [user, setUser] = useState<User>(createAnonymousUser());
+    const [user, setUser] = useState<User>(activeUserExists() ? getActiveUser() : createAnonymousUser());
 
     return (
         <AuthContext.Provider value={{ user, setUser }}>
